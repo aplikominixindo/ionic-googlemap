dffd7360f2d18f558c6648fa4cb77eb1773f5096
Aplikom Inixindo <aplikominixindo@gmail.com>
2018-04-27 16:03:27 +0700

    commit this

--- src/pages/googlemaps/googlemaps.ts
+++ src/pages/googlemaps/googlemaps.ts
@@ -1,7 +1,16 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { GoogleMap, GoogleMapsEvent, GoogleMapsLatLng }
- from '@ionic-native/google-maps';
+import { IonicPage, NavController, NavParams, Platform } from 'ionic-angular';
+import { GoogleMap, GoogleMaps }
+  from '@ionic-native/google-maps';
+import { ViewChild, ElementRef } from '@angular/core';
+import {
+  CameraPosition,
+  LatLng,
+  GoogleMapsEvent,
+  Marker, MarkerOptions
+} from '@ionic-native/google-maps';
+import { Geolocation } from '@ionic-native/geolocation';
+import { mapStyle } from '../home/mapStyle';
 
 @IonicPage()
 @Component({
@@ -9,12 +18,58 @@ import { GoogleMap, GoogleMapsEvent, GoogleMapsLatLng }
   templateUrl: 'googlemaps.html',
 })
 export class GooglemapsPage {
+  @ViewChild('map') mapElement: ElementRef;
+  private map: GoogleMap;
+  private location: LatLng;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController,
+    public navParams: NavParams,
+    public platform: Platform) {
+    //this.loadMap();
+    this.location = new LatLng(-7.3221912, 112.7449952);
   }
 
+  /*loadMap() {
+    let location = new LatLng(-7.3221912, 112.7449952);
+  }*/
+
   ionViewDidLoad() {
-    console.log('ionViewDidLoad GooglemapsPage');
+    this.platform.ready().then(() => {
+      console.log('ionViewDidLoad GooglemapsPage');
+
+      let element = this.mapElement.nativeElement;
+      this.map = GoogleMaps.create(element);
+
+      this.map.one(GoogleMapsEvent.MAP_READY).then(() => {
+        let options = {
+          target: this.location,
+          zoom: 15
+        };
+        
+        this.map.moveCamera(options);
+        setTimeout(() => {
+          this.addMarker()
+        }, 3000);
+      });
+    });
+
+  }
+
+  addMarker() {
+    this.map.addMarker({
+      title: 'Lokasiku',
+      icon: 'blue',
+      animation: 'DROP',
+      position: {
+        lat: this.location.lat,
+        lng: this.location.lng
+      }
+    })
+      .then(marker => {
+        marker.on(GoogleMapsEvent.MARKER_CLICK).subscribe(() => {
+          alert('Lokasiku diklik!');
+        });
+      });
   }
 
 }
